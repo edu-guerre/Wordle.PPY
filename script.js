9d7b8c4b21b73b0df5966eca764d1e2cfdda6052
edu-guerre <160611972+edu-guerre@users.noreply.github.com>
2024-02-28 23:41:17 -0300

    Wordle 5 letras

---
+++ script.js
@@ -0,0 +1,87 @@
+let intentos = 6;
+let opciones = ["NACER","SIETE","MARZO","ABRIL","ROBIN","MARIO","LUIGI","SUPER"]
+const palabra = opciones[Math.floor(Math.random() * opciones.length)];
+
+let gameover = false;
+let errorletra = false;
+window.addEventListener('load', init)
+
+const button = document.getElementById("Intentar-boton");
+const input = document.getElementById("guess-input");
+const valor = input.value;
+const GRID = document.getElementById("grid");
+
+button.addEventListener("click",()=>{
+    verificar();
+    if (verificar()==true){
+        alert("Debe contener 5 letras");
+    }else if (gameover==false){
+        intentar();
+    }else{
+        reintentar();
+    }
+});
+
+
+function init() {
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+function intentar() {
+    const INTENTO = leerIntento();
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i] === palabra[i]) { //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if (palabra.includes(INTENTO[i])) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+    intentos--
+    if (intentos == 0) {
+        terminar("<h1>PERDISTE!😖</h1>")
+    }
+    if (INTENTO === palabra) {
+        terminar("<h1>GANASTE!😀</h1>")
+    }
+}
+function verificar(){
+    let intento = document.getElementById("guess-input");
+    let contenedor = new Array(5);
+    intento= intento.value;
+    for(let i=0;i<contenedor.length;i++){
+        contenedor[i]=intento[i];
+        if(contenedor[i]===undefined || contenedor[i]===null){
+            errorletra = true;
+            return errorletra;
+        }
+    }
+}
+function leerIntento() {
+    let intento = document.getElementById("guess-input");
+    intento= intento.value;
+    intento = intento.toUpperCase();
+    return intento;
+}
+function terminar(mensaje) {
+    const INPUT = document.getElementById("guess-input");
+    gameover = true;
+    INPUT.disabled = true;
+    let contenedor = document.getElementById('intentos');
+    contenedor.innerHTML = mensaje;
+    button.id="reintentar";
+    button.innerText="Reintentar"
+}
+function reintentar(){
+    location.reload();
+}
+
